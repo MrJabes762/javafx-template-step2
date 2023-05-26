45aab4cb4bc67791275c29f557405b1cbf8be301
leandro-costa <leandro.costa@ifba.edu.br>
2023-05-26 20:06:33 -0300

    master e logout

--- src/main/java/br/edu/ifba/saj/fwads/controller/MasterController.java
+++ src/main/java/br/edu/ifba/saj/fwads/controller/MasterController.java
@@ -1,23 +1,25 @@
 package br.edu.ifba.saj.fwads.controller;
 
+import br.edu.ifba.saj.fwads.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 
 public class MasterController {
 
     @FXML
-    private Button btnManu1;
-
-    @FXML
-    private Button btnManu2;
-
-    @FXML
     private BorderPane masterPane;
+
+    @FXML
+    private VBox menu;
 
     @FXML
     private Label userEmail;
@@ -27,17 +29,38 @@ public class MasterController {
 
     @FXML
     void logOff(MouseEvent event) {
-
+        Alert alert = new Alert(AlertType.CONFIRMATION, "Deseja realmente sair??", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait()
+            .filter(response -> response == ButtonType.YES)
+            .ifPresent(response -> {
+                App.setRoot("controller/Login.fxml");            
+            });
     }
 
     @FXML
     void showHome(ActionEvent event) {
-
+        limparBotoes(event.getSource());        
     }
 
     @FXML
     void showUsuarios(ActionEvent event) {
+        limparBotoes(event.getSource());
+    }
 
+    private void limparBotoes(Object source){
+        menu.getChildren().forEach((node) -> 
+            {
+                if(node instanceof Button btn){
+                    btn.getStyleClass().clear();
+                    btn.getStyleClass().add("btn-menu");
+                }
+            }
+
+        );
+        if(source instanceof Button btn){
+            btn.getStyleClass().clear();
+            btn.getStyleClass().add("btn-menu-selected");
+        }
     }
 
 }
